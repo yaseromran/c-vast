e3824c1ebdae28f245fac292a718e586aa901d6d
yaser <engineeryaseromran@gmail.com>
2019-11-06 10:11:49 +0200

    Contact Form use case : Models send message

--- back/database/migrations/2019_09_30_074958_edit_column_recieved_emails_table.php
+++ back/database/migrations/2019_09_30_074958_edit_column_recieved_emails_table.php
@@ -16,21 +16,12 @@ class EditColumnRecievedEmailsTable extends Migration
         Schema::table('recieved_emails', function (Blueprint $table)
         {
 //-
-
-
             $table->integer('delete_by_admin_user_id')->unsigned()->nullable()->change();
             $table->integer('last_admin_comment_id')->unsigned()->nullable()->change();
             $table->integer('last_admin_done_email_log_id')->unsigned()->nullable()->change();
             $table->integer('last_admin_open_log_id')->unsigned()->nullable()->change();
             $table->integer('last_admin_replied_email_id')->unsigned()->nullable()->change();
             $table->integer('last_admin_restore_email_log_id')->unsigned()->nullable()->change();
-
-
-
-
-
-
-
         });
     }
 
